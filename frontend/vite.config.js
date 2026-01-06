6617277ae9c4eddd6a4435e4799cab7caeee3b5d
SEOZZZ04 <150577136+SEOZZZ04@users.noreply.github.com>
2026-01-06 18:50:40 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../static', // 상위 폴더의 static으로 빌드
-    emptyOutDir: true
+    outDir: '../static',
+    emptyOutDir: true,
   }
 })
